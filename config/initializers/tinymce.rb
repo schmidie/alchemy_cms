c2a278c9659d09270cec7593239dc7f1964ab52a
Tobias Schmid <2toschmid@googlemail.com>
2025-04-28 12:59:48 +0200

    show main_menu in navbar

--- config/initializers/tinymce.rb
+++ config/initializers/tinymce.rb
@@ -1,18 +1,18 @@
 Alchemy::Tinymce.init = {
      plugins: [
-    'a11ychecker', 'accordion', 'advlist', 'anchor', 'autolink', 'autosave',
-    'charmap', 'code', 'codesample', 'directionality', 'emoticons', 'exportpdf',
-    'exportword', 'fullscreen', 'help', 'image', 'importcss', 'importword',
-    'insertdatetime', 'link', 'lists', 'markdown', 'math', 'media', 'nonbreaking',
-    'pagebreak', 'preview', 'quickbars', 'save', 'searchreplace', 'table',
-    'visualblocks', 'visualchars', 'wordcount'
+    "a11ychecker", "accordion", "advlist", "anchor", "autolink", "autosave",
+    "charmap", "code", "codesample", "directionality", "emoticons", "exportpdf",
+    "exportword", "fullscreen", "help", "image", "importcss", "importword",
+    "insertdatetime", "link", "lists", "markdown", "math", "media", "nonbreaking",
+    "pagebreak", "preview", "quickbars", "save", "searchreplace", "table",
+    "visualblocks", "visualchars", "wordcount"
   ],
-    toolbar: 'undo redo | accordion accordionremove | ' +
-    'importword exportword exportpdf | math | ' +
-    'blocks fontfamily fontsize | bold italic underline strikethrough | ' +
-    'align numlist bullist | link image | table media | ' +
-    'lineheight outdent indent | forecolor backcolor removeformat | ' +
-    'charmap emoticons | code fullscreen preview | save print | ' +
-    'pagebreak anchor codesample | ltr rtl',
-  menubar: 'file edit view insert format tools table help'
+    toolbar: "undo redo | accordion accordionremove | " +
+    "importword exportword exportpdf | math | " +
+    "blocks fontfamily fontsize | bold italic underline strikethrough | " +
+    "align numlist bullist | link image | table media | " +
+    "lineheight outdent indent | forecolor backcolor removeformat | " +
+    "charmap emoticons | code fullscreen preview | save print | " +
+    "pagebreak anchor codesample | ltr rtl",
+  menubar: "file edit view insert format tools table help"
   }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount Alchemy::Engine, at: '/'
+  mount Alchemy::Engine, at: "/"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
