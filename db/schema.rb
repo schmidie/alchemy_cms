37c91e2ffcb643ef7aaeb9593e0ce751e76ba88c
Tobias Schmid <2toschmid@googlemail.com>
2025-04-28 14:44:46 +0200

    use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_04_24_114928) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "alchemy_attachments", force: :cascade do |t|
     t.string "name"
     t.string "file_name"
